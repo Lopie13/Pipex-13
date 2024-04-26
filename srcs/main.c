45da1623e16267d2c49c22b7c5cf5a52a93f6b19
Lopie13 <miguelmatalvesbarradas@gmail.com>
2024-04-26 21:30:43 +0100

    main done question mark

--- srcs/main.c
+++ srcs/main.c
@@ -1,4 +1,26 @@
-int	main(argc, argv[])
+#include "../pipex.h"
+
+int	main(int argc, char *argv[])
 {
+	int fd[2];
+	int pid1;
+
+	if (argc == 5)
+	{
+		if (pipe(fd) == -1)
+			return 1;
+		pid1 = fork();
+		if (pid1 < 0)
+			return 2;
+		if (pid1 == 0)
+			child_process();
+		waitpid(pid1, NULL, 0);
+		parent_process();
+	}
+	else
+	{
+		printf("Error: Bad arguments\n");
+		printf("Ex: ./pipex <file1> <cmd1> <cmd2> <file2>\n");
+	}
 	return (0);
 }
