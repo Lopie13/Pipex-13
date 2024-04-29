a24c42555db1eece3ae074a289f6512ddc2a98d6
Lopie13 <miguelmatalvesbarradas@gmail.com>
2024-04-29 11:10:38 +0100

    yesyes

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,12 +1,33 @@
 #include "../pipex.h"
 
-void	command(char argv[2], char **envp)
-{
 	//does the specified commands somehow...
 	//using split maybe? or idk
+void	command(char *argv, char **envp)
+{
+	char	*path;
+	char	*cmd;
+	int	melon;
+
+	cmd = ft_split(argv);
+	path = find_path(cmd[0], envp);
+	if (!path)	
+	{
+		while (cmd[melon])
+		{
+			free(cmd[melon]);
+			melon++;
+		}
+		free(cmd);
+		error();
+	}
 }
 
 char	*find_path(char *cmd, char **envp)
 {
 	//somehow finds the path to the commands we need?
+}
+
+void	error()
+{
+	//prints error and quits
 }
